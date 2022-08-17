e9f575a1edd2f7182c57aec1352dd1265cd47a9b
Evillyn da Silva Oliveira <evillyn.oliveira@sensedia.local>
2022-08-16 23:23:10 -0300

    first commit

---
+++ springrestful/src/main/java/br/com/springrestful/Startup.java
@@ -0,0 +1,14 @@
+package br.com.springrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		System.out.println("ataa2");
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
