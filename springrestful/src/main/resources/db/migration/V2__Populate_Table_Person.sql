b2a203e0422c5a51c3603d7ed815cc67b5e340e6
ev1illyn <evillyndsoliveiras@gmail.com>
2022-09-08 16:19:43 -0300

    add migration with flyway

---
+++ springrestful/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `first_name`, `last_name`, `address`, `gender`) VALUES
+    (1,'loki','q te morde','tiradentes','macho'),
+    (2,'evy','q te sova','tiradentes','muie'),
+    (6,'loki atualiza e deleta','q te sova','tiradentes','macho'),
+    (7,'loki deleta','q te sova','tiradentes','macho');
